cf1731ce8b12c74091ce1a89b466d26e88d21a53
Kip Lawrence <kip.lawrence@domo.com>
2016-11-08 14:33:33 -0700

    Basic UI put in place.

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var messageToUser: UILabel!
 
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("touched")
+    }
 }
 
